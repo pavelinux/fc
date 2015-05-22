ae42f83a614547780a76884b15b4432c91f5585f
pavelinux <pavel.oropeza@me.com>
2015-05-22 16:43:03 -0500

    Actualizo el programa. Ya incluye las sumas de fuerzas F_x y F_y. Falta calcular el potencial de corte

--- s4/md1.f90
+++ s4/md1.f90
@@ -1,11 +1,11 @@
 MODULE VARIABLES
 IMPLICIT NONE
 INTEGER :: N,NN
-REAL*8 :: RHO,LX,LY,DX,DY,UPOT,M_X,M_Y 
+REAL*8 :: RHO,LX,LY,DX,DY,RIJ,UPOT,ULJ,M_X,M_Y 
 REAL*8 :: SGM = 1.0, EPS=1.0, RCUT=2.50
 REAL*8, DIMENSION(:), ALLOCATABLE::RX,RY
 REAL*8, DIMENSION(:), ALLOCATABLE::VX,VY
-!REAL*8, DIMENSION(:), ALLOCATABLE::FX,FY
+REAL*8, DIMENSION(:), ALLOCATABLE::FX,FY
 REAL*8 RND
 END MODULE VARIABLES
 
@@ -14,7 +14,7 @@ USE VARIABLES
 IMPLICIT NONE
 CALL COORDENADAS
 CALL CELDA
-!CALL FUERZA
+CALL FUERZA
 END PROGRAM MD_F
 
 SUBROUTINE COORDENADAS
@@ -25,7 +25,7 @@ WRITE(*,*) 'DAME EL NUMERO DE ATOMOS'
 READ(*,*) N
 ALLOCATE(RX(N),RY(N))
 ALLOCATE(VX(N),VY(N))
-!ALLOCATE(FX(N),FY(N))
+ALLOCATE(FX(N),FY(N))
 WRITE(*,*) 'DAME LA DENSIDAD'
 READ(*,*) RHO
 LX = SQRT(DBLE(N/RHO))
@@ -73,31 +73,35 @@ END DO
 CLOSE(1)
 END SUBROUTINE CELDA
 
-!SUBROUTINE FUERZA
-!USE VARIABLES
-!IMPLICIT NONE
-!DO I=1, N
-!    FX(I) = 0.0
-!    FY(I) = 0.0
-!END DO
-!UPOT = 0.0
-!DO I = 1, NN - 1
-! DO J = I+1, NN
-!     IF(I.NE.J) THEN
-!        DX = RX(I) - RX(J)
-!        DY = RY(I) - RY(J)
-!        RIJ = SQRT(DX * DX + DY * DY)
-!        IF (RIJ<= RCUT) THEN
-!            ULJ = 4.0 * EPS * ((SGM/RIJ)**6 * ( (SGM/RIJ)**6 - 1.0)) -UCUT
-!            UPOT = UPOT + ULJ
-!            DULJ = 48.0 * EPS * (SGM/RIJ)**6 * ((SGM/RIJ)**6 -0.5)/(RIJ * RIJ)
-!            FX(I) = FX(I) + DULJ * DX
-!            FY(I) = FY(I) + DULJ * DY
-!            FX(J) = FY(J) + DULJ * DY
-!            FY(J) = FX(J) + DULJ * DY
-!        END IF
-!     END IF
-! END DO 
-!END DO
+SUBROUTINE FUERZA
+USE VARIABLES
+IMPLICIT NONE
+INTEGER :: I, J
+REAL*8 :: DULJ
+DO I=1, N
+    FX(I) = 0.0
+    FY(I) = 0.0
+END DO
+UPOT = 0.0
+DO I = 1, NN - 1
+ DO J = I+1, NN
+     IF(I.NE.J) THEN
+        DX = RX(I) - RX(J)
+        DY = RY(I) - RY(J)
+        RIJ = SQRT(DX * DX + DY * DY)
+        IF (RIJ<= RCUT) THEN
+!            ULJ = 4.0 * EPS * ((SGM/RIJ)**6 * ( (SGM/RIJ)**6 - 1.0)) - UCUT
+            ULJ = 4.0 * EPS * ((SGM/RIJ)**6 * ( (SGM/RIJ)**6 - 1.0)) 
+            UPOT = UPOT + ULJ
+            DULJ = 48.0 * EPS * (SGM/RIJ)**6 * ((SGM/RIJ)**6 - 0.5)/(RIJ * RIJ)
+            FX(I) = FX(I) + DULJ * DX
+            FY(I) = FY(I) + DULJ * DY
+            FX(J) = FY(J) + DULJ * DY
+            FY(J) = FX(J) + DULJ * DY
+        END IF
+     END IF
+ END DO 
+END DO
 !WRITE(*,*) sum(fx),sum(y),ucut
-!END SUBROUTINE FUERZA
+WRITE(*,*) sum(fx),sum(fy)
+END SUBROUTINE FUERZA
