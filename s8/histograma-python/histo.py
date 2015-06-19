12fbbe7e647e2c68204ec9820e7010a0c1ac3238
pavelinux <zahori@gmail.com>
2015-06-19 01:10:05 -0500

    Se verifica perfil de distribucion gaussiano para velocidades finales. El histograma quedo en python

---
+++ s8/histograma-python/histo.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.patches as patches
+import matplotlib.path as path
+
+fig, ax = plt.subplots()
+
+# charge data
+# previous do: awk {'print $2'} velocidades_al_final.dat > vy_al_final.dat
+
+data = open('vy_al_final.dat','r')
+# Convert from a list of strings to a list of floats
+data = map(float, data) 
+
+# compare values from the list and array
+ls = max(data)
+li = min(data)
+print ls, li
+# we charge nat (as n) and number of bins
+n, bins = np.histogram(data, 100)
+
+left = np.array(bins[:-1])
+print left
+right = np.array(bins[1:])
+print right
+
+bottom = np.zeros(len(left))
+top = bottom + n
+
+XY = np.array([[left,left,right,right], [bottom,top,top,bottom]]).T
+
+# Rectangles with path and patch
+barpath = path.Path.make_compound_path_from_polys(XY)
+
+patch = patches.PathPatch(barpath, facecolor='green', edgecolor='gray', alpha=0.6)
+ax.add_patch(patch)
+
+# update the view limits
+ax.set_xlim(left[0], right[-1])
+ax.set_ylim(bottom.min(), top.max())
+
+plt.show()
--- s8/histograma.f90
+++ s8/histograma.f90
@@ -1,11 +1,13 @@
 PROGRAM HISTOGRAMA
 IMPLICIT NONE
-REAL*8 :: DELTA,LI,LS,Vx_i
-REAL*8, DIMENSION(1000)::VX
+REAL*8 :: DELTA,LI,LS,VX_I
+INTEGER, DIMENSION(:), ALLOCATABLE::VX
 INTEGER, DIMENSION(:), ALLOCATABLE::FREQ
-INTEGER :: I, BIN, NO_BINS, NAT=1000
-NO_BINS = 100
-!ALLOCATE(FREQ(-50,50))
+INTEGER :: I, BIN, NO_BINS=100, NAT=1000, SUMA_FREQ
+
+ALLOCATE(FREQ(NAT))
+ALLOCATE(VX(NAT))
+
 DO I = 1, NAT
     OPEN(1,FILE='vx_al_final.dat',STATUS='OLD',ACTION='READ')
     READ(1, *) VX(i)
@@ -14,22 +16,29 @@ CLOSE(1)
 
 LS = MAXVAL(VX)
 LI = MINVAL(VX)
-WRITE(*,*) LS, LI
 DELTA = (LS - LI) / NO_BINS
-!write(*,*) delta
 
-!DO I = 1, NAT
-!    OPEN(1,FILE='vx_al_final.dat',STATUS='OLD',ACTION='READ')
-!    VX_i = VX_i + 1.0
-!    BIN = VX_i / DELTA
-!    FREQ(BIN) = FREQ(BIN) + 1
-!end do
-!close(1)
-!
-!DO I = 0, NO_BINS
-!    OPEN(2,FILE='salida_histograma_vx_final.dat',STATUS='UNKNOWN',ACTION='WRITE')
-!    WRITE(2,*) I * DELTA - 1, FREQ(I)
-!END DO
-!CLOSE(2)
+DO I = 1, NAT
+    OPEN(1,FILE='vx_al_final.dat',STATUS='OLD',ACTION='READ')
+    READ(1,*) VX_I
+    VX_i = VX_i + 1.0
+    BIN = VX_i / DELTA
+    write(*,*) vx_i, bin
+    !FREQ(BIN) = FREQ(BIN) + 1
+END DO
+CLOSE(1)
 
+suma_freq = 0.0
+DO I = 0, NO_BINS
+    OPEN(2,FILE='salida_histograma_vx_final.dat',STATUS='UNKNOWN',ACTION='WRITE')
+    suma_freq = freq(i) + suma_freq
+    WRITE(2,*) I * DELTA - 1, FREQ(I)
+END DO
+CLOSE(2)
+
+if(suma_freq .ne. NAT) then
+    write(*,*) 'wrong data. check your code!', suma_freq
+else
+    write(*,*) suma_freq
+endif
 END PROGRAM HISTOGRAMA
